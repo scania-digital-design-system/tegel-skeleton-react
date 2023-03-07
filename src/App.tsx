57289e134ec97bdb43706c7c57a8c965d4cc790e
mJarsater <martin.jarsater@hotmail.com>
2023-03-07 16:28:17 +0100

    minor cleanup, favicon and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Logo from './logo.svg'
 
 function App() {
   return (
     <div className="App">
       <div className="sdds-headline-01 sdds-u-center">Welcome to Tegel</div>
+      <img className='logo' src={Logo} alt="Scania Logo" />
     </div>
   );
 }
